b06b78804c125e01646497e8702e7636c0ec6cb3
Moandor <moandor.y@gmail.com>
2026-03-12 18:50:39 -0500

    FeralDruidBot: overhaul combat logic

--- TestBot.cs/TestBot.cs
+++ TestBot.cs/TestBot.cs
@@ -43,14 +43,10 @@ namespace TestBot
         {
             ThreadSynchronizer.RunOnMainThread(() =>
             {
-                {
-                    Console.WriteLine(Functions.LuaCallWithResult(@"
-                        {0} = RealmList:IsShown()
-                        if {0} then
-                            RealmList_OnOk()
-                        end
-                    ")[0]);
-                }
+                WoWUnit target = ObjectManager
+                    .Units
+                    .Single(u => u.Guid == ObjectManager.Player.TargetGuid);
+                target.Interact();
             });
         }
     }
